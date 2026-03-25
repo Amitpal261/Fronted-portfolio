db02db84a2310efdeb330756ec6c3d013cb85064
Amit pal <amit.pal336900@gmail.com>
2026-03-25 09:37:05 +0530

    Fix router import and paths for Netlify

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -8,46 +8,28 @@ import AuthPage from "../pages/AuthPage";
 import AdminProjectUpload from "../pages/AdminProjectUpload";
 import ProjectDetails from "../pages/ProjectDetails";
 
-
 const router = createBrowserRouter([
-      {
-        path: "projects/:id",
-        element: <ProjectDetails/>
-      },
-     {
-        path: "admin",
-        element: <AuthPage />
-      },
-       {
-        path: "upload",
-        element: <AdminProjectUpload />
-      },
+  {
+    path: "projects/:id",
+    element: <ProjectDetails />
+  },
+  {
+    path: "admin",
+    element: <AuthPage />
+  },
+  {
+    path: "upload",
+    element: <AdminProjectUpload />
+  },
   {
     path: "/",
     element: <App />,
     children: [
-       {
-        index: true, // ✅ default route
-        element: <Home />,
-      },
-      {
-        path: "/home",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-      {
-        path: "contact",
-        element: <Contact />
-      },
-      {
-        path: "projects",
-        element: <Projects />
-      },
-  
-    
+      { index: true, element: <Home /> },
+      { path: "home", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+      { path: "projects", element: <Projects /> }
     ]
   }
 ]);
